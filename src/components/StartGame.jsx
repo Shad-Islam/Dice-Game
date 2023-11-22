3f1ca1c9988b6119e2e8b0a9339057b1a8f09823
Shad-islam <shadislam48@gmail.com>
2023-11-23 03:00:56 +0600

    Complete start game page

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const StartGame = () => {
-  return <div className="start">StartGame</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="home-images">
+          <img src="/images/dices.png" alt="dices" />
+        </div>
+        <div className="home-textArea">
+          <h1>DICE GAME</h1>
+          <button className="button">Play Now</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default StartGame;
