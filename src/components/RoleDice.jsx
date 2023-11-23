a2e5eecc0f6c86add2e369c7e5656ea5e6ff72cb
Shad-islam <shadislam48@gmail.com>
2023-11-24 02:14:54 +0600

    Add funtionality on Role dice component

--- src/components/RoleDice.jsx
+++ src/components/RoleDice.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RoleDice() {
+  const [currentDice, setCurrentDice] = useState(1);
+
+  let rendomNumber = () => {
+    let num = Math.floor(Math.random() * 6) + 1;
+    return setCurrentDice(num);
+  };
+  console.log(currentDice);
+
   return (
-    <div className="container">
-      <div className="RoleDice">
-        <img src="./images/dice_1.png" alt="" />
+    <div div className="role-dice">
+      <div className="dice" onClick={() => rendomNumber()}>
+        <img src={`./images/dice_${currentDice}.png`} alt="dice" />
       </div>
+      <p>Click on Dice to roll</p>
     </div>
   );
 }
